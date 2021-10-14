080f6986128f4d722e91a1ea05d4c8e4c081acb5
xemerald <b98204032@gmail.com>
2021-10-14 20:36:17 +0800

    Try to finish the chan_mod script.

--- scripts/chan_mod.sh
+++ scripts/chan_mod.sh
@@ -7,7 +7,7 @@ SRC_DIR=${WORKING_DIR}/data_src
 RES_DIR=${WORKING_DIR}/data_res
 
 #
-if [ $# -ne 9 ]
+if [ $# -ne 6 ]
 then
 	echo "Usage: chan_mod.sh <Year> <Month> <Day> <Hour> <Minute> <Second>"
 	exit 0
@@ -21,7 +21,7 @@ SAC_DIR=''
 #
 for _EV_HM in ${EV_HM}
 do
-	SAC_DIR=`cd ${FILEPATH}; ls ${EV_YMD}_${_EV_HM}*_MAN 2> /dev/null`
+	SAC_DIR=`cd ${FILEPATH}; ls | grep ${EV_YMD}_${_EV_HM}*_MAN`
 	SAC_DIR=${SAC_DIR//[[:space:]]/}
 	if [ ${SAC_DIR} ] && [ -e "${FILEPATH}/${SAC_DIR}" ]
 	then
@@ -47,14 +47,20 @@ cp -R ${FILEPATH}/${SAC_DIR} ${TMP_DIR}
 #echo "Decompressing the archived SAC files..."
 #tar -C ${TMP_DIR} -vjxf ${TMP_DIR}/${SAC_DIR}
 #rm -f ${TMP_DIR}/${SAC_DIR}
+echo "Listing all the P-Alert archived SAC files..."
 FILELIST=`cd ${TMP_DIR}/${SAC_DIR}; ls | grep "[A-Z][0-9]\{2,3\}[A-F]\{0,1\}.HH[ZNE].TW.--"`
 for file in ${FILELIST}
 do
 	NEW_CHAN=`echo ${file} | cut -d. -f2 | sed 's/HH/HL/'`
 	chan_mod ${TMP_DIR}/${SAC_DIR}/${file} ${NEW_CHAN} ${TMP_DIR}/${SAC_DIR}
 	rm -f ${TMP_DIR}/${SAC_DIR}/${file}
-	break
 done
+#
+echo "Compressing all archived SAC files..."
+cd ${TMP_DIR}; tar -vjcf ${SAC_DIR}.tar.bz2 ${SAC_DIR}; cd -
+#
+echo "Deleting the temporary folder..."
+rm -rf ${TMP_DIR}/${SAC_DIR}
 #
 #
 echo "Event processing complete!!"
